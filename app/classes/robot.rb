9acc3bf4a2e5213df6a7ea599289216387cd5049
adamgoodapp <adamgoodapp@gmail.com>
2016-02-17 14:47:08 +0000

    git as

--- app/classes/robot.rb
+++ app/classes/robot.rb
@@ -2,15 +2,15 @@ require 'inline'
 
   class Robot
 
-    inline(:C) do |builder|
-      # builder.include '</home/rails/rails_project/app/classes/CGIKcore.cpp>'
-      # builder.add_compile_flags '-x c++', '-lstdc++'
-      #
-      # builder.c "
-      # int hello() {
-      #   return 1;
-      # }
-    # "
-    end
+    # inline(:C) do |builder|
+    #   # builder.include '</home/rails/rails_project/app/classes/CGIKcore.cpp>'
+    #   # builder.add_compile_flags '-x c++', '-lstdc++'
+    #   #
+    #   # builder.c "
+    #   # int hello() {
+    #   #   return 1;
+    #   # }
+    # # "
+    # end
 
   end
