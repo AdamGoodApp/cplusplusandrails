6f1617455b10ea4fcd67050db196dbc66aa63706
adamgoodapp <adamgoodapp@gmail.com>
2016-02-17 14:27:47 +0000

    sd

--- app/classes/robot.rb
+++ app/classes/robot.rb
@@ -1,16 +1,16 @@
-# require 'inline'
-#
-#   class Robot
-#
-#     inline(:C) do |builder|
-#       builder.include '</home/rails/rails_project/app/classes/CGIKcore.cpp>'
-#       builder.add_compile_flags '-x c++', '-lstdc++'
-#
-#       builder.c "
-#       double hello() {
-#         return MathTypes::Constants::pi;
-#       }
-#     "
-#     end
-#
-#   end
+require 'inline'
+
+  class Robot
+
+    inline(:C) do |builder|
+      builder.include '</home/rails/rails_project/app/classes/CGIKcore.cpp>'
+      builder.add_compile_flags '-x c++', '-lstdc++'
+
+      builder.c "
+      double hello() {
+        return MathTypes::Constants::pi;
+      }
+    "
+    end
+
+  end
